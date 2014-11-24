3a7909f0a336eb3767a0cd2c6c7bed6912bc297f
daddybear1208 <daddybear1208@gmail.com>
2014-11-24 22:36:04 +0800

    vertex array

--- src/main.cpp
+++ src/main.cpp
@@ -1,52 +1,41 @@
 #include <iostream>
+#ifdef _WIN32
 #include <Windows.h>
+#endif
+#include <GL/glew.h>
 #include <GL/GL.h>
 #include <GL/GLU.h>
 #include <GL/freeglut.h>
 
-static GLfloat spin = 0.0;
+using namespace std;
+
+static GLfloat vertex[] = { 10, 10, 0,
+					200, 10, 0,
+					200, 200, 0,
+					10, 200, 0};
+
+static GLfloat colors[] = {255,0,0,
+						255,0,0,
+						0,255,0,
+						0,255,0};
+
+GLbyte indices[] = { 0, 1, 2, 3 };
 
 void init()
 {
 	glClearColor(0.0, 0.0, 0.0, 0.0);
-	glShadeModel(GL_FLAT);
+	glEnableClientState(GL_VERTEX_ARRAY);
+	glEnableClientState(GL_COLOR_ARRAY);
+	
 }
 
 void display()
 {
-	glClear(GL_COLOR_BUFFER_BIT);
-	glPushMatrix();
-	glRotatef(spin, 0.0, 0.0, 1.0);
-	glColor3f(1.0, 0.0, 0.0);
-	glutWireTeapot(10);
-	glPopMatrix();
+	glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT);
+	glVertexPointer(3, GL_FLOAT, 0, vertex);
+	glColorPointer(3, GL_FLOAT, 0, colors);
+	glDrawArrays(GL_QUADS, 0, 4);
 	glutSwapBuffers();
-	
-}
-
-void spinDisplay()
-{
-	spin += 2.0;
-	if (spin > 360.0)
-		spin -= 360.0;
-	glutPostRedisplay();
-}
-
-void mouse(int button, int state, int x, int y)
-{
-	switch (button)
-	{
-	case GLUT_LEFT_BUTTON:
-		if (state == GLUT_DOWN)
-			glutIdleFunc(spinDisplay);
-		break;
-	case GLUT_RIGHT_BUTTON:
-		if (state == GLUT_DOWN)
-			glutIdleFunc(NULL);
-		break;
-	default:
-		break;
-	}
 }
 
 void reshape(int w, int h)
@@ -54,25 +43,28 @@ void reshape(int w, int h)
 	glViewport(0, 0, GLsizei(w), GLsizei(h));
 	glMatrixMode(GL_PROJECTION);
 	glLoadIdentity();
-	glOrtho(-50.0, 50.0, -50.0, 50.0, -1.0, 1.0);
-	glMatrixMode(GL_MODELVIEW);
-	glLoadIdentity();
+	gluOrtho2D(0.0, GLdouble(w), 0.0, GLdouble(h));
 }
-
-
 
 
 int main(int argc, char* argv[])
 {
 	glutInit(&argc, argv);
 	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB);
-	glutInitWindowSize(250, 250);
+	glutInitWindowSize(512, 512);
 	glutInitWindowPosition(100, 100);
+	//glutInitContextVersion(4, 3);
+	//glutInitContextProfile(GLUT_CORE_PROFILE);
 	glutCreateWindow("OpenGL");
+	if (glewInit())
+	{
+		std::cerr << "Unable to initialize GLEW, exiting" << std::endl;
+		exit(EXIT_FAILURE);
+	}
+	
 	init();
-	glutDisplayFunc(&display);
+	glutDisplayFunc(display);
 	glutReshapeFunc(reshape);
-	glutMouseFunc(mouse);
 	glutMainLoop();
 	return 0;
 }
